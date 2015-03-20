0f778a6c947da0b1ce4e2cd99811028091209bca
Davide Vernizzi <davide.vernizzi@gmail.com>
2015-03-20 17:27:32 +0000

    Bar controller

---
+++ protected/controllers/BarController.php
@@ -0,0 +1,49 @@
+<?php
+
+class BarController extends Controller
+{
+	/**
+	 * Declares class-based actions.
+	 */
+	public function actions()
+	{
+		return array(
+			// captcha action renders the CAPTCHA image displayed on the contact page
+			'captcha'=>array(
+				'class'=>'CCaptchaAction',
+				'backColor'=>0xFFFFFF,
+			),
+			// page action renders "static" pages stored under 'protected/views/site/pages'
+			// They can be accessed via: index.php?r=site/page&view=FileName
+			'page'=>array(
+				'class'=>'CViewAction',
+			),
+		);
+	}
+
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+        echo 'GET bar';
+	}
+
+    public function actionCreate()
+    {
+        echo 'POST bar';
+    }
+
+    public function actionUpdate()
+    {
+        echo 'PUT bar';
+    }
+
+    public function actionDelete()
+    {
+        echo 'DELETE bar';
+    }
+}
